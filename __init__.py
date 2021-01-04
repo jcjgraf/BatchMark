d5a0940a69197034d508be69c8ab1503f231ae50
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2021-01-04 20:42:52 +0100

    IMPROVE: Redactor plugin
    - Wrap functionality in BatchMark class
    - Add toggle functionality with `batchmark-toggle`
    - Rename start and end command to `batchmark-start` and `batchmark-end`

--- __init__.py
+++ __init__.py
@@ -8,57 +8,81 @@ from vimiv.utils import log
 
 _logger = log.module_logger(__name__)
 
-start_index = None
-start_paths = None
-
 library = MainWindow.instance._library
 
 
-@api.commands.register()
-def batchmarkstart() -> None:
-    """Start Batch Mark Selection.
+class BatchMark:
+    @api.objreg.register
+    def __init__(self) -> None:
+        self.start_index = None
+        self.end_index = None
+        self.paths = None
 
-    Starts the selection at the currently selected image."""
-    global start_paths, start_index
+    @api.commands.register()
+    def batchmark_start(self) -> None:
+        """Start Batch Mark Selection.
 
-    start_paths = library.model().paths
-    currentPath = library.current()
-    start_index = library.model().paths.index(currentPath)
+        Starts the selection at the currently selected image."""
 
+        self.paths = api.pathlist()
+        current_path = api.current_path()
+        self.start_index = self.paths.index(current_path)
 
-@api.commands.register()
-def batchmarkend() -> None:
-    """End Batch Mark Selection.
+    @api.commands.register()
+    def batchmakr_end(self) -> None:
+        """End Batch Mark Selection.
 
-    Ends the selection at the currently selected image. If at least one image in the
-    selection is unmarked, all images get markded. If all images are already marked,
-    they all get unmarked."""
-    global start_paths, start_index
+        Ends the selection at the currently selected image. If at least one image in the
+        selection is unmarked, all images get markded. If all images are already marked,
+        they all get unmarked."""
 
-    end_paths = library.model().paths
+        end_paths = library.model().paths
 
-    if start_paths != end_paths:
-        return
+        if self.paths is None or self.paths != end_paths:
+            self.paths = None
+            return
 
-    currentPath = library.current()
-    end_index = library.model().paths.index(currentPath)
+        current_path = api.current_path()
+        self.end_index = self.paths.index(current_path)
 
-    selected_paths = end_paths[
-        min(start_index, end_index) : max(start_index, end_index) + 1
-    ]
+        self.batchmarktoggle()
 
-    unmarked = False  # If true, at least one image in selected_paths is not marked
+    @api.commands.register()
+    def batchmark_toggle(self) -> None:
+        """Toggles the currently selected region.
 
-    # If all images are maked, unmark them, else mark them
-    for path in selected_paths:
-        try:
-            api.mark._unmark(path)
-        except ValueError:
-            unmarked = True
-            continue
+        If at least one image in the selection is unmarked, all images get markded. If
+        all images are already marked, they all get unmarked."""
 
-    # By now all images are unmarked. Mark all agin in case ther was at least
-    # one unmarked image in the beginning
-    if unmarked:
+        if self.paths is None or self.start_index is None or self.end_index is None:
+            self.paths = None
+            return
+
+        selected_paths = self.paths[self._getLowerBound() : self._getUpperBound()]
+
+        unmarked = False  # If true, at least one image in selected_paths is not marked
+
+        # If all images are maked, unmark them, else mark them
         for path in selected_paths:
-            api.mark._mark(path)
+            try:
+                api.mark._unmark(path)
+            except ValueError:
+                unmarked = True
+                continue
+
+        # By now all images are unmarked. Mark all agin in case ther was at least
+        # one unmarked image in the beginning
+        if unmarked:
+            for path in selected_paths:
+                api.mark._mark(path)
+
+    def _getLowerBound(self):
+        return min(self.start_index, self.end_index)
+
+    def _getUpperBound(self):
+        return max(self.start_index, self.end_index) + 1
+
+
+def init(*_args: Any, **_kwargs: Any) -> None:
+    """Initialize BatchMark class."""
+    BatchMark()
