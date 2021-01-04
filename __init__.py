8b70ebebc0bc8ef28bb939f6f34156a14b58ae8d
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2021-01-04 20:42:52 +0100

    IMPROVE: Redactor plugin
    - Wrap functionality in BatchMark class
    - Use vimiv api
    - Add toggle functionality with `batchmark-toggle`
    - Rename start and end command to `batchmark-start` and `batchmark-end`

--- __init__.py
+++ __init__.py
@@ -3,62 +3,86 @@
 from typing import Any
 
 from vimiv import api
-from vimiv.gui.mainwindow import MainWindow
 from vimiv.utils import log
 
 _logger = log.module_logger(__name__)
 
-start_index = None
-start_paths = None
 
-library = MainWindow.instance._library
+class BatchMark:
+    @api.objreg.register
+    def __init__(self) -> None:
+        self.start_index = None
+        self.end_index = None
+        self.paths = None
 
+    @api.commands.register()
+    def batchmark_start(self) -> None:
+        """Start Batch Mark Selection.
 
-@api.commands.register()
-def batchmarkstart() -> None:
-    """Start Batch Mark Selection.
+        Starts the selection at the currently selected image."""
 
-    Starts the selection at the currently selected image."""
-    global start_paths, start_index
+        self.paths = api.pathlist()
+        current_path = api.current_path()
+        self.start_index = self.paths.index(current_path)
 
-    start_paths = library.model().paths
-    currentPath = library.current()
-    start_index = library.model().paths.index(currentPath)
+    @api.commands.register()
+    def batchmark_end(self) -> None:
+        """End Batch Mark Selection.
 
+        Ends the selection at the currently selected image. If at least one image in the
+        selection is unmarked, all images get markded. If all images are already marked,
+        they all get unmarked."""
 
-@api.commands.register()
-def batchmarkend() -> None:
-    """End Batch Mark Selection.
+        end_paths = api.pathlist()
 
-    Ends the selection at the currently selected image. If at least one image in the
-    selection is unmarked, all images get markded. If all images are already marked,
-    they all get unmarked."""
-    global start_paths, start_index
+        if self.paths is None or self.paths != end_paths:
+            self.paths = None
+            return
 
-    end_paths = library.model().paths
+        current_path = api.current_path()
+        self.end_index = self.paths.index(current_path)
 
-    if start_paths != end_paths:
-        return
+        selected_paths = self.paths[self._getLowerBound() : self._getUpperBound()]
 
-    currentPath = library.current()
-    end_index = library.model().paths.index(currentPath)
+        unmarked = False  # If true, at least one image in selected_paths is not marked
 
-    selected_paths = end_paths[
-        min(start_index, end_index) : max(start_index, end_index) + 1
-    ]
-
-    unmarked = False  # If true, at least one image in selected_paths is not marked
-
-    # If all images are maked, unmark them, else mark them
-    for path in selected_paths:
-        try:
-            api.mark._unmark(path)
-        except ValueError:
-            unmarked = True
-            continue
-
-    # By now all images are unmarked. Mark all agin in case ther was at least
-    # one unmarked image in the beginning
-    if unmarked:
+        # If all images are maked, unmark them, else mark them
         for path in selected_paths:
-            api.mark._mark(path)
+            try:
+                api.mark._unmark(path)
+            except ValueError:
+                unmarked = True
+                continue
+
+        # By now all images are unmarked. Mark all agin in case ther was at least
+        # one unmarked image in the beginning
+        if unmarked:
+            for path in selected_paths:
+                api.mark._mark(path)
+
+    @api.commands.register()
+    def batchmark_toggle(self) -> None:
+        """Starts and ends batchmark selection.
+
+        If not batchmark selection has been started or the last one is no longer valid
+        (e.g. when the path was changed) a batch mark is started. Else the batch mark
+        is ended and if at least one image in the selection is unmarked, all images get
+        markded. If all images are already marked, they all get unmarked."""
+
+        if self.paths is None or self.start_index is None:
+            self.batchmark_start()
+
+        elif self.end_index is None:
+            self.batchmark_end()
+            self.paths = None
+
+    def _getLowerBound(self):
+        return min(self.start_index, self.end_index)
+
+    def _getUpperBound(self):
+        return max(self.start_index, self.end_index) + 1
+
+
+def init(*_args: Any, **_kwargs: Any) -> None:
+    """Initialize BatchMark class."""
+    BatchMark()
