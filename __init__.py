9a04be9c5cabf7132874f2c062f3fa07c95ad16d
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2022-01-15 22:39:06 +0100

    IMPROVE: ditch STATUS as a boolean is sufficient
    also add some debug statements

--- __init__.py
+++ __init__.py
@@ -12,11 +12,6 @@ _logger = log.module_logger(__name__)
 
 
 class BatchMark(QObject):
-    class STATUS:
-        invalid = 0
-        idle = 1
-        started = 2
-
     @api.objreg.register
     def __init__(self) -> None:
         super().__init__()
@@ -27,16 +22,24 @@ class BatchMark(QObject):
         api.signals.event_handled.connect(self._batchmark_update)
         api.working_directory.handler.loaded.connect(self.batchmark_cancel)
 
+        _logger.debug("Initialized BatchMark")
+
     def _batchmark_update(self):
         """Update the marked paths according to the current position."""
-        if self.get_status() != self.STATUS.started:
+        if not self._is_started():
+            _logger.info("Need to start selection first")
             return
 
         current_path = api.current_path()
         self.end_index = self.paths.index(current_path)
 
         selected_paths = set(
-            self.paths[self._get_lower_bound() : self._get_upper_bound()]
+            self.paths[
+                min(self.start_index, self.end_index) : max(
+                    self.start_index, self.end_index
+                )
+                + 1
+            ]
         )
 
         no_longer_marked = self._triggered - selected_paths
@@ -63,10 +66,13 @@ class BatchMark(QObject):
             self._reverse_action = api.mark.Action.Unmark
         self.start_index = self.paths.index(current_path)
 
+        _logger.debug("New selection started")
+
     @api.commands.register()
     def batchmark_accept(self) -> None:
         """Accept the changes of the current Batch Mark Selection."""
         self._reset()
+        _logger.debug("Selection accepted")
 
     @api.commands.register()
     def batchmark_toggle(self) -> None:
@@ -75,44 +81,35 @@ class BatchMark(QObject):
         In case we are currently in Batch Mark, accept the changes. Otherwise start
         Batch Mark."""
 
-        if self.get_status() == self.STATUS.started:
+        if self._is_started():
             self.batchmark_accept()
-            self._reset()
         else:
             self.batchmark_start()
 
     @api.commands.register()
     def batchmark_cancel(self) -> None:
         """Cancels the Batch Mark Selection if started."""
-        if self.get_status() == self.STATUS.started:
+        if self._is_started():
             to_mark = self._triggered & self._prev_marked
             to_unmark = self._triggered - self._prev_marked
             api.mark.mark(to_mark, api.mark.Action.Mark)
             api.mark.mark(to_unmark, api.mark.Action.Unmark)
             self._reset()
+            _logger.debug("Selection cancelled")
+        else:
+            _logger.debug("Nothing to cancel")
 
-    def get_status(self):
-
+    def _is_started(self):
+        """Indicate if a selection has been started."""
         if self.paths is None or self.start_index is None:
-            return self.STATUS.idle
+            return False
 
-        paths = api.pathlist()
-
-        if paths != self.paths:
-            return self.STATUS.invalid
-
-        return self.STATUS.started
+        return True
 
     @api.status.module("{batchmark}")
     def batchmark(self) -> str:
-        status = self.get_status()
-
-        if status == self.STATUS.started:
+        if self._is_started():
             color = styles.get("base0d")
-            return wrap_style_span(f"color: {color}", "<b>+</b>")
-
-        if status == self.STATUS.invalid:
-            color = styles.get("base08")
             return wrap_style_span(f"color: {color}", "<b>+</b>")
 
         return ""
@@ -123,12 +120,6 @@ class BatchMark(QObject):
         self.paths = None
         self._prev_marked = set()
         self._triggered = set()
-
-    def _get_lower_bound(self):
-        return min(self.start_index, self.end_index)
-
-    def _get_upper_bound(self):
-        return max(self.start_index, self.end_index) + 1
 
 
 def init(*_args: Any, **_kwargs: Any) -> None:
